a37e502e7c86016fb834f65ac0f1c63056a76e1d
danielstocker <mail@danielstocker.net>
2016-11-01 11:40:02 +0000

    initial commit

---
+++ ContosoFront/ContosoFront/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ContosoFront.Startup))]
+namespace ContosoFront
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
